f05571204cf6880d2cb8a7164698791fccbbc0ab
bal3000 <bal3000286@googlemail.com>
2021-04-25 16:20:15 +0100

    tests finished

--- handlers/producthandler_test.go
+++ handlers/producthandler_test.go
@@ -10,33 +10,74 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type mockJsonDataStore struct{}
+type mockDataStore struct{}
 
-func (ds mockJsonDataStore) PackSizesForProduct(productId string) ([]int, bool, error) {
+func (ds mockDataStore) PackSizesForProduct(productId string) ([]int, bool, error) {
 	return []int{250, 500, 1000, 2000, 5000}, true, nil
 }
 
 type mockResponse struct {
-	Pack500 int `json:"500"`
+	Pack250  int `json:"250"`
+	Pack500  int `json:"500"`
+	Pack1000 int `json:"1000"`
+	Pack2000 int `json:"2000"`
+	Pack5000 int `json:"5000"`
+}
+
+type testCase struct {
+	input          string
+	expectedRes    mockResponse
+	expectedStatus int
 }
 
 func TestCalculatePacksToSend(t *testing.T) {
-	body := `{
-		"productId": "123",
-		"itemsOrder": 251
-	}`
-	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(body))
-	rec := httptest.NewRecorder()
-
-	ph := ProductHandler{dataStore: mockJsonDataStore{}}
-	ph.CalculatePacksToSend(rec, req)
-
-	expectedRes := mockResponse{
-		Pack500: 1,
+	var tests = []testCase{
+		{input: `{
+			"productId": "123",
+			"itemsOrder": 1
+		}`, expectedRes: mockResponse{
+			Pack250: 1,
+		}, expectedStatus: 200},
+		{input: `{
+			"productId": "123",
+			"itemsOrder": 250
+		}`, expectedRes: mockResponse{
+			Pack250: 1,
+		}, expectedStatus: 200},
+		{input: `{
+			"productId": "123",
+			"itemsOrder": 251
+		}`, expectedRes: mockResponse{
+			Pack500: 1,
+		}, expectedStatus: 200},
+		{input: `{
+			"productId": "123",
+			"itemsOrder": 501
+		}`, expectedRes: mockResponse{
+			Pack250: 1,
+			Pack500: 1,
+		}, expectedStatus: 200},
+		{input: `{
+			"productId": "123",
+			"itemsOrder": 12001
+		}`, expectedRes: mockResponse{
+			Pack250:  1,
+			Pack2000: 1,
+			Pack5000: 2,
+		}, expectedStatus: 200},
 	}
-	actualRes := mockResponse{}
-	json.NewDecoder(rec.Body).Decode(&actualRes)
 
-	assert.Equal(t, http.StatusOK, rec.Code)
-	assert.Equal(t, expectedRes, actualRes)
+	for _, tc := range tests {
+		req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(tc.input))
+		rec := httptest.NewRecorder()
+
+		ph := ProductHandler{dataStore: mockDataStore{}}
+		ph.CalculatePacksToSend(rec, req)
+
+		actualRes := mockResponse{}
+		json.NewDecoder(rec.Body).Decode(&actualRes)
+
+		assert.Equal(t, tc.expectedStatus, rec.Code)
+		assert.Equal(t, tc.expectedRes, actualRes)
+	}
 }
