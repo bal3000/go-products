937f298b3730bbd633fbb366eff20c66d2fdd491
bal3000 <bal3000286@googlemail.com>
2021-04-28 09:57:17 +0100

    revert

--- main.go
+++ main.go
@@ -1,26 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/bal3000/go-products/app"
 	"github.com/bal3000/go-products/storage"
 )
 
 func main() {
-	ds, err := storage.NewJSON("./packsizes.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// username := os.Getenv("MONGO_USERNAME")
-	// password := os.Getenv("MONGO_PASSWORD")
-
-	// ds, closer, err := storage.NewMongo(fmt.Sprintf("mongodb+srv://%s:%s@baltest.4maj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", username, password))
+	// ds, err := storage.NewJSON("./packsizes.json")
 	// if err != nil {
 	// 	log.Fatalln(err)
 	// }
-	// defer closer()
+
+	username := os.Getenv("MONGO_USERNAME")
+	password := os.Getenv("MONGO_PASSWORD")
+
+	ds, closer, err := storage.NewMongo(fmt.Sprintf("mongodb+srv://%s:%s@baltest.4maj7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", username, password))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer closer()
 
 	srv := app.NewServer(ds)
 
